c6b8d9ff1a1949087c13bfd9a4eb5b51b4286ad4
Parchello <parchellos@gmail.com>
2023-12-27 13:10:21 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { AboutPage, ContactPage, HomePage, ProjectPage } from "./Pages";
+
+function App() {
+  return (
+    <main>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/contacts" element={<ContactPage />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import ContactPage from "./ContactPage";
+import ProjectPage from "./ProjectPage";
+
+export { HomePage, AboutPage, ContactPage, ProjectPage };
