2c56abcb5f94b0d8f2c12cab1921c68716781409
Parchello <parchellos@gmail.com>
2023-12-27 14:16:53 +0200

    added island

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../Components/Loader";
+import Island from "../Models/island";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -9,13 +10,34 @@ import Loader from "../Components/Loader";
 }
 
 const HomePage = () => {
+  const adjustIslandScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition] = adjustIslandScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island position={islandPosition} scale={islandScale} />
+        </Suspense>
       </Canvas>
     </section>
   );
